ac4a6e3febc5f23fa119ffd3c1d2b96f8435eb65
ZB <biyuhuaping@163.com>
2025-06-13 12:08:50 +0800

    支持URL Scheme跳转
    集成广告，并支持URL Scheme跳转打开广告页面

--- CardTrove/CardTroveApp.swift
+++ CardTrove/CardTroveApp.swift
@@ -1,12 +1,51 @@
-
-
 import SwiftUI
+import AppLovinSDK
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct CardTroveApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @State private var showAdPage = false
+
+    init() {
+        let initConfig = ALSdkInitializationConfiguration(sdkKey: "LNR7IqQoTN_ruXr55ZWa_0SoWtyL65IFWSneUVVlGsv6RXs6idmUqtaf7AilM7UX_9NOyitGTFk_0prZ75JyhZ") { builder in
+            builder.mediationProvider = ALMediationProviderMAX
+        }
+        ALSdk.shared().initialize(with: initConfig) { sdkConfig in
+            print("AppLovin SDK initialized.")
+            // 可以在这里预加载广告，例如 Interstitials
+            let _ = InterstitialAdVC.shared
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            if showAdPage {
+                AdLaunchView()
+            } else {
+                MainTabView()
+                    .onOpenURL { url in
+                        handleIncomingURL(url)
+                    }
+            }
+        }
+    }
+    
+    // 通用的URL处理方法：CardTroveApp://page?name=AppLovin
+    private func handleIncomingURL(_ url: URL) {
+        if let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+           let queryItems = components.queryItems,
+           queryItems.contains(where: { $0.name == "name" && $0.value == "AppLovin" }) {
+            print("接收到链接: \(url.absoluteString)")
+            print("queryItems: \(queryItems)")
+            print("Path: \(url.path)")
+            showAdPage = true
         }
     }
 }
